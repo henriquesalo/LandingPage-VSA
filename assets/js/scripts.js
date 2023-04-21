bba793f96d78ea390d253fdb8964bc147b4a900c
henriquesalo <henriquersalomao@gmail.com>
2023-04-21 12:33:03 -0300

    att

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+$("#menu").click(function(){
+    $(".background").css("display","flex");
+});
+$("#close").click(function(){
+    $(".background").css("display","none");
+});
+=======
+$("#menu").click(function(){
+    $(".background").css("display","flex");
+});
+$("#close").click(function(){
+    $(".background").css("display","none");
+});
+>>>>>>> 1646d27 (att)
